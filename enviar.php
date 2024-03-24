f86aaa1852523ed806a979096094fb0089bdd90b
LR4f <163931749+LR4f@users.noreply.github.com>
2024-03-24 04:28:08 -0300

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -4,7 +4,7 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
 $mensagem = $_POST['mensagem'];
-$From = 'atendimento@lr4f.github.io.'
+$From = 'atendimento@lr4f.github.io.';
 
 $headers = "MIMI-Version: 1.1\n";
 $headers .= "Content-type: text/html; charset=utf-8\n";
@@ -12,6 +12,6 @@ $headers .= "From: TESTE <$From>\n";
 $headers .= "Return-Path: $From\n";
 $headers .= "Reply-to: $email";
 
-mail($email, $mensagem, $headers, $From);
+mail($email, $assunto, $mensagem, $headers, $From);
 
 ?>
