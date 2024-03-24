3612381942d57aa9aed2b3fd31c5295ad999eb68
LR4f <163931749+LR4f@users.noreply.github.com>
2024-03-24 04:14:09 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
+
+echo $nome.'</br>';
+echo $email.'</br>';
+echo $assunto.'</br>';
+echo $mensagem.'</br>';
+
+?>
